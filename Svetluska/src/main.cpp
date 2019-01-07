86a1668e616928d0b001f555f2418f07cd4e4855
itak1614 <46463860+itak1614@users.noreply.github.com>
2019-01-08 00:13:38 +0100

    Add files via upload

---
+++ Svetluska/src/main.cpp
@@ -0,0 +1,71 @@
+#include <Arduino.h>
+int buttonPin=10;
+int buttonState;
+int switcher=0;
+int i=1;
+int r=2;
+int g=3;
+int b=4;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(r,OUTPUT);
+  pinMode(g,OUTPUT);
+  pinMode(b,OUTPUT);
+  pinMode(buttonPin,INPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  buttonState=digitalRead(buttonPin);
+switch (i) {
+  case 1:
+      digitalWrite(r,HIGH);
+      digitalWrite(g,HIGH);
+      digitalWrite(b,HIGH);
+      delay(1500);
+      digitalWrite(r,LOW);
+      digitalWrite(g,LOW);
+      digitalWrite(b,LOW);
+      i++;
+    break;
+  case 2:
+      digitalWrite(r,HIGH);
+      delay(1500);
+      digitalWrite(r,LOW);
+      i++;
+    break;
+  case 3:
+      digitalWrite(g,HIGH);
+      delay(1500);
+      digitalWrite(g,LOW);
+      i++;
+    break;
+  case 4:
+      digitalWrite(b,HIGH);
+      delay(1500);
+      digitalWrite(b,LOW);
+      i++;
+    break;
+  case 5:
+  do{
+      digitalWrite(r,HIGH);
+      delay(500);
+      digitalWrite(r,LOW);
+      digitalWrite(g,HIGH);
+      delay(500);
+      digitalWrite(g,LOW);
+      digitalWrite(b,HIGH);
+      delay(500);
+      digitalWrite(b,LOW);
+      switcher++;
+    }while(switcher==3);
+      i=1;
+      switcher=0;
+    break;
+    default:
+      digitalWrite(r,LOW);
+      digitalWrite(g,LOW);
+      digitalWrite(b,LOW);
+      }
+}
